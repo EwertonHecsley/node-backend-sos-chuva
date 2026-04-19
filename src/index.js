0740ee902b8415605c773fd8a92b58556ac8eba4
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2026-04-19 17:57:29 -0300

    chore: instaled cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+
 const router = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
 
 app.use('/api/v1', router);
 
